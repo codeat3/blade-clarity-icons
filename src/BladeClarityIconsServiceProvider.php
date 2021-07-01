3e0d38a1b0b6008cb4f59fa1280d5b6cd8a9282f
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2021-07-01 03:02:14 +0000

    Fix styling

--- src/BladeClarityIconsServiceProvider.php
+++ src/BladeClarityIconsServiceProvider.php
@@ -19,7 +19,6 @@ final class BladeClarityIconsServiceProvider extends ServiceProvider
 
             $factory->add('clarity', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
-
     }
 
     private function registerConfig(): void
@@ -39,5 +38,4 @@ final class BladeClarityIconsServiceProvider extends ServiceProvider
             ], 'blade-clarity-icons-config');
         }
     }
-
 }
